3bc26388fff9d256c06663580e588f3522fa182f
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-05-22 23:54:36 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,5 +11,37 @@ public class Main
 {
 	public static void main( String args[] ) throws IOException
 	{
+		File file = new File("maze.dat");
+    Scanner sc = new Scanner(file);
+    //testing
+    int size = sc.nextInt();
+    sc.nextLine();
+    Maze maze = new Maze(size, sc.nextLine());
+    System.out.println(maze);
+
+    size = sc.nextInt();
+    sc.nextLine();
+    maze = new Maze(size, sc.nextLine());
+    System.out.println(maze);
+
+    size = sc.nextInt();
+    sc.nextLine();
+    maze = new Maze(size, sc.nextLine());
+    System.out.println(maze);
+
+    size = sc.nextInt();
+    sc.nextLine();
+    maze = new Maze(size, sc.nextLine());
+    System.out.println(maze);
+    
+    size = sc.nextInt();
+    sc.nextLine();
+    maze = new Maze(size, sc.nextLine());
+    System.out.println(maze);
+
+    size = sc.nextInt();
+    sc.nextLine();
+    maze = new Maze(size, sc.nextLine());
+    System.out.println(maze);
 	}
 }
