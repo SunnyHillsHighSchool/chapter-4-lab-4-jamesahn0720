fd660520cf221c8d81614f750c06568856ec7efb
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-05-22 23:55:19 -0700

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -13,18 +13,44 @@ public class Maze
 
 	public Maze(int size, String line)
 	{
-
+Scanner sc = new Scanner(line);
+    maze = new int[size][size];
+    for(int i = 0; i < size; i++)
+    {
+      for(int j = 0; j < size; j++)
+      {
+        maze[i][j] = sc.nextInt();
+      }
+    }
 
 	}
 
 	public boolean hasExitPath(int r, int c)
 	{
+		if(c==maze[0].length-1)
+      if(maze[r][c]==1)
+        return true;
+    
+    if(r>=0 && r<maze.length && maze[r][c]==1)
+    {
+        return (hasExitPath(r,c+1) || hasExitPath(r+1,c));
+    }
 		return false;
 	}
 
 	public String toString()
 	{
-		String output="";
-		return output;
+	String output = "";
+    for(int r = 0; r < maze.length; r++)
+    {
+      for(int c = 0; c < maze[0].length; c++)
+      {
+        output += maze[r][c]+ " ";
+      }
+      output+= "\n";
+    }
+    if(hasExitPath(0,0))
+		  return output + "exit found\n";
+		return output + "exit not found\n";
 	}
 }
